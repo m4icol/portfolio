e327087ce34abf55d7d50d46c30174a25a78acb1
Maickol <m4icol.r12@gmail.com>
2024-11-02 16:33:30 -0500

    new project added and fixed some sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				"CS-Dark-Background": "hsl(0, 0, 5)",
-				"CS-Dark-Secondary": "hsl(0, 0, 10)",
-				"CS-Dark-Header": "hsl(0, 0, 6)",
-				"CS-Dark-Font": "hsl(0, 0, 100)",
-				"CS-Dark-Border": "hsl(0, 0, 50)",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "CS-Dark-Background": "hsl(0, 0, 8)",
+        "CS-Dark-Secondary": "hsl(0, 0, 10)",
+        "CS-Dark-Header": "hsl(0, 0, 7)",
+        "CS-Dark-Font": "hsl(0, 0, 100)",
+        "CS-Dark-Border": "hsl(0, 0, 50)",
 
-				"CS-Light-Background": "hsl(0, 0, 98)",
-				"CS-Light-Secondary": "hsl(0, 0, 0)",
-				"CS-Light-Header": "hsl(0, 0, 97)",
-				"CS-Light-Font": "hsl(0, 0, 0)",
-				"CS-Light-Border": "hsl(0, 0, 0)"
-			}
-		},
-	},
-	plugins: [],
-}
+        "CS-Light-Background": "hsl(0, 0, 95)",
+        "CS-Light-Secondary": "hsl(0, 0, 0)",
+        "CS-Light-Header": "hsl(0, 0, 94)",
+        "CS-Light-Font": "hsl(0, 0, 0)",
+        "CS-Light-Border": "hsl(0, 0, 0)",
+      },
+    },
+  },
+  plugins: [],
+};
